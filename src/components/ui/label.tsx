60e27e8870e4fc1f3e21b0fa8a3f42d1c3504374
Gphinx <pincagenesis@gmail.com>
2025-09-17 06:45:12 +0800

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export default function Label(){
-  return (
-    <div className='p-3 bg-gray-800 rounded'>
-      <strong>Label</strong>
-      <div className='text-gray-400 text-sm'>Placeholder for label</div>
-    </div>
-  )
-}
+import { cn } from '@/lib/utils';
+
+const labelVariants = cva('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70');
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
+>(({ className, ...props }, ref) => <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />);
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
