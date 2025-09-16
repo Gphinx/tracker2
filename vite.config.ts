46ed891cd3b3c915fc69ee7792270567a6cc7397
Gphinx <pincagenesis@gmail.com>
2025-09-17 07:20:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({plugins:[react()]});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { viteSourceLocator } from "@metagptx/vite-plugin-source-locator";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    viteSourceLocator({
+      prefix: "mgx",
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
