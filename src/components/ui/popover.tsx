7989c6b9ee8c68dd8e916532bf12a3b8cdae7951
Gphinx <pincagenesis@gmail.com>
2025-09-17 06:47:48 +0800

    Update popover.tsx

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import * as React from 'react';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
 
-export default function Popover(){
-  return (
-    <div className='p-3 bg-gray-800 rounded'>
-      <strong>Popover</strong>
-      <div className='text-gray-400 text-sm'>Placeholder for popover</div>
-    </div>
-  )
-}
+import { cn } from '@/lib/utils';
+
+const Popover = PopoverPrimitive.Root;
+
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+const PopoverContent = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn(
+        'z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        className
+      )}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+));
+PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
+export { Popover, PopoverTrigger, PopoverContent };
